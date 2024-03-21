5a155a69c04d7dd3d9cff2856b1cab100f3868ed
DaggyJ <tranxporterdaggy@gmail.com>
2024-03-22 00:37:36 +0300

    commit changes

--- CSS/04 - Borders & Background/README.md
+++ CSS/04 - Borders & Background/README.md
@@ -1,4 +1,4 @@
-# CSS Borders
+git# CSS Borders
 
 The CSS border properties allow you to specify the style, width, and color of an element's border.
 
--- JavaScript/Project 7/script.js
+++ JavaScript/Project 7/script.js
@@ -1,21 +1,27 @@
-// PROJECT Description:
-/*
-* You are tasked with manipulating a simple webpage using JavaScript.
-* The webpage contains a paragraph (<p>) element with the id paragraph and an empty div element with the id imageContainer.
-* Your goal is to select the paragraph element, change its content to "Hello World", and then create an image element (<img>) with the source attribute set to "image.jpg" and append it to the imageContainer div.
 
-// INSTRUCTIONS
-
-/* Task 1:
- *Select the paragraph element with the id paragraph and change its content to "Hello World".
-
-/* Task 2:
-* Using JavaScript, create an image element with the source attribute set to "image.jpg".
-
-/* Task 3:
-* Append the created image element to the div element with the id imageContainer.
-
-
-*/
-
-// Write your code here. SUCCESS
+  
+     // Task 1: Select the paragraph element with the id paragraph and change its content to "Hello World". 
+     const paragraphElement = document.getElementById('paragraph'); 
+     if (paragraphElement) { 
+       paragraphElement.textContent = 'Hello World'; 
+       
+     } 
+     let p = document.getElementsByTagName('p')
+     console.log(p[0]);
+     p[0].style.color = 'tomato'
+     p[0].style.fontSize = '48px'
+     console.log(p[1]);
+     p[1].innerText = 'Douglas J. Ongoma'
+     p[1].style.color = 'purple'
+     p[1].style.fontSize = '20px'
+     // Task 2: Using JavaScript, create an image element with the source attribute set to "image.jpg". 
+     const imageElement = document.createElement('img'); 
+     imageElement.src = 'image.jpg'; 
+     //img.style.width = '100%'
+     //img.style.height = 'auto'
+  
+     // Task 3: Append the created image element to the div element with the id imageContainer. 
+     const imageContainer = document.getElementById('imageContainer'); 
+     if (imageContainer) { 
+       imageContainer.appendChild(imageElement); 
+     }
